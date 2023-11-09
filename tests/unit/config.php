11e25af1a9291ed73f3c6ac67493b626144b6dbf
devpastet <devpastet@yandex.ru>
2023-11-09 17:40:23 +0300

    Init

--- tests/unit/config.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-return [
-    'id' => 'sentry-tests',
-    'class' => 'yii\console\Application',
-    'basePath' => \Yii::getAlias('@tests'),
-    'runtimePath' => \Yii::getAlias('@tests/_output'),
-    'bootstrap' => [],
-];
